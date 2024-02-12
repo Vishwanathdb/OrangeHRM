3671e158e325aad256708e4c39d46be3454f6153
Vishwanath D B <vishwanathdb2000@gmail.com>
2024-02-13 01:38:25 +0530

    Completed fetching data from the excel sheet

---
+++ src/test/java/VishLimited/excelDriven/DataDriven.java
@@ -0,0 +1,79 @@
+package VishLimited.excelDriven;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataDriven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ArrayList<ArrayList<String>> getExcelData() throws IOException {
+
+		// Fetch Path of the file
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\Data Source.xlsx");
+
+		// Create excel object
+		XSSFWorkbook workBook = new XSSFWorkbook(fis);
+
+		ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+
+		// Number of sheets in the excel
+		int numberOfSheets = workBook.getNumberOfSheets();
+
+		// Scanning all the sheets
+		for (int i = 0; i < numberOfSheets; i++) {
+
+			// Access sheet with name "credentials"
+			if (workBook.getSheetName(i).equals("credentials")) {
+
+				// Create sheet object
+				XSSFSheet sheet = workBook.getSheetAt(i);
+
+				// Fetch all rows
+				Iterator<Row> rows = sheet.rowIterator();
+
+				// Scan each row
+				while (rows.hasNext()) {
+					ArrayList<String> rowData = new ArrayList<String>();
+
+					// Get present row
+					Row row = rows.next();
+
+					// Fetch all cells
+					Iterator<Cell> cells = row.cellIterator();
+
+					// Scan each cell
+					while (cells.hasNext()) {
+
+						// Get present cell
+						Cell cell = cells.next();
+
+						// Save the cell value
+						rowData.add(cell.getCellType() == CellType.STRING ? cell.getStringCellValue()
+								: NumberToTextConverter.toText(cell.getNumericCellValue()));
+					}
+
+					// Save the row value
+					data.add(rowData);
+				}
+			}
+		}
+
+		// return data from excel
+		return data;
+
+	}
+
+}
--- src/test/java/VishLimited/orangeHRM/HRAdministrationPageModel.java
+++ src/test/java/VishLimited/orangeHRM/HRAdministrationPageModel.java
@@ -1,20 +1,22 @@
 package VishLimited.orangeHRM;
 
+import VishLimited.excelDriven.DataDriven;
 import VishLimited.pageObject.CreateHRAdmin;
 import VishLimited.pageObject.EditHRAdmin;
 import VishLimited.pageObject.HRAdminPage;
 import VishLimited.pageObject.RemoveHRAdmin;
 import VishLimited.testComponent.BaseTest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class HRAdministrationPageModel extends BaseTest {
 	
-	@Test()
-	void main() throws InterruptedException {
-		
-		String userName = "Admin", password = "OYs6MbnC2@";
-		String employeeToAdd = "Aaron";
-		String newUserName = "Aaron Update";
+	@Test(dataProvider = "dataSource")
+	void main(String userName, String password, String employeeToAdd, String newUserName) throws InterruptedException {
 		
 		HRAdminPage hrObject = login.loginToSite(userName, password);
 		
@@ -27,6 +29,21 @@ public class HRAdministrationPageModel extends BaseTest {
 		remove.deleteHRAdimn();
 		
 		
+	}
+	
+	@DataProvider(name = "dataSource")
+	public Object[][] getDataFromExcel () throws IOException{
+		DataDriven excelObj = new DataDriven();
+		
+		ArrayList<ArrayList<String>> data = excelObj.getExcelData();
+		
+		Object[][] object = new Object[data.size()-1][data.get(0).size()];
+		
+		for(int i=1; i<data.size(); i++)
+			for(int j=0; j<data.get(0).size(); j++)
+				object[i-1][j] = data.get(i).get(j);
+		
+		return object;
 	}
 	
 }
